f53be772fdd08f30cddeea9be07741dc3aafb54c
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-01-20 12:28:30 -0300

    Corrigindo Bug quantidades

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -2,13 +2,9 @@ let precos = [];
 let precoMultiplicado = [];
 let val = document.querySelectorAll('.val');
 
-
 $('document').ready(function () {
 
     listandoPrecos();
-
-
-
 });
 
 function listandoPrecos() {
@@ -16,30 +12,31 @@ function listandoPrecos() {
     $('.val').each(function (index, element) {
         precos.push(element.innerHTML);
     });
-}
-
+};
 
 function multiplicandoPreco() {
 
     var unidade = document.getElementById('qtd');
     var valorIndex = unidade.options[unidade.selectedIndex].value;
 
-
     precos.forEach(function (element) {
+
         var multplica = parseFloat(element) * valorIndex;
-
         precoMultiplicado.push(multplica);
+
     });
+};
 
-}
+function atualizandoPrecos(){
 
-
-function update() {
-    
-    multiplicandoPreco();
-    
     for (var i = 0; i < val.length; i++) {
         val[i].textContent = precoMultiplicado[i].toFixed(2);
     }
+};
 
+function update() {
+
+    multiplicandoPreco();
+    atualizandoPrecos();
+    precoMultiplicado = [];
 };
